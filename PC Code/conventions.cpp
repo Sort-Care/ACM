20cca4684b42acb60aa1a6c470efb7554217a292
sort-care <hji@umass.edu>
2018-10-01 08:45:15 -0400

    uva11926

--- PC Code/conventions.cpp
+++ PC Code/conventions.cpp
@@ -6,4 +6,6 @@ typedef vector<ii> vii;
 typedef vector<int> vi;
 #define INF 1000000000
 
+#define REP(i, a, b) for (int i = int(a); i <= int(b); i++)
+
 
---
+++ PC Code/uva11926.cpp
@@ -0,0 +1,28 @@
+/*
+ * Uva 11926
+ * Author: Haoyu Ji
+ * Input: 
+ *       First line: n m (n: the number of one-time task)
+ *                       (m: the number of repeating task)
+ *       Following n lines:
+ *                         each line: two num, start and end time
+ *       Following m lines: start, end, and interval
+ * All numbers are integers in the range [0..1000000]
+ * For each task, the end time is guranteed to be larger than the 
+ * start time, and repetition interval is larger than 0.
+ * Input terminates with: 0 0
+ */
+
+
+
+#include <stdio.h>
+#include <algorithm>
+#include <math>
+
+def REP(i,a,b) for (int i = int(a); i <= int(b); i++);
+
+
+int main(){
+    
+    return 1;
+}
